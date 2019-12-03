de64aba1b39b150473597a7434f4005f51871918
C. Spencer Beggs <spencer@beg.gs>
2019-12-03 08:44:47 -0500

    Adds day 1, part 2

--- src/day-1/index.js
+++ src/day-1/index.js
@@ -4,11 +4,28 @@ function calculateFuel(mod) {
   return Math.floor(mod / 3) - 2;
 }
 
-async function main() {
+function part1() {
   let totalFuel = mods.reduce((acc, mod) => {
     return acc + calculateFuel(mod);
   }, 0);
-  console.log(totalFuel);
+  console.log(`Answer part 1: ${totalFuel}`);
+}
+
+function part2() {
+  let totalFuel = mods.reduce((acc, mod, index, arr) => {
+    let extraFuel = calculateFuel(mod);
+    while (extraFuel > 0) {
+      acc = acc + extraFuel;
+      extraFuel = calculateFuel(extraFuel);
+    }
+    return acc;
+  }, 0);
+  console.log(`Answer part 2: ${totalFuel}`);
+}
+
+async function main() {
+  part1();
+  part2();
 }
 
 main();
